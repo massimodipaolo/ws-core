e4765feed34e907d0d1b995c4c5668565d7b9a47
massimodipaolo <massimodipaolo@users.noreply.github.com>
2018-01-22 12:46:56 +0100

    Api expose protected cached repository

--- src/modules/Api/Controllers/EntityCached.cs
+++ src/modules/Api/Controllers/EntityCached.cs
@@ -6,45 +6,45 @@ namespace core.Extensions.Api.Controllers
 {
     [Route("api/cache/[controller]")]
     public class EntityCachedController<T> : EntityController<T> where T : IEntity
-    {        
-        private readonly ICacheRepository<T> _cached;
+    {
+        protected ICacheRepository<T> _cachedRepository;
 
         public EntityCachedController(IRepository<T> repository,ICacheRepository<T> cached) : base(repository)
         {
-            _cached = cached;
+            _cachedRepository = cached;
         }
 
         [HttpGet]
         public override IActionResult Get()
         {
-            return Ok(_cached.List);
+            return Ok(_cachedRepository.List);
         }
 
         [HttpGet("{id}")]
         public override IActionResult Get(string id)
         {
-            return Ok(_cached.Find(id));
+            return Ok(_cachedRepository.Find(id));
         }
 
         [HttpPost]
         public override void Post([FromBody]T entity)
         {                        
-            _cached.Add(entity);            
-            base.Post(entity);
+            _cachedRepository.Add(entity);            
+            //base.Post(entity);
         }
 
         [HttpPut("{id}")]
         public override void Put(string id, [FromBody]T entity)
         {
-            _cached.Update(entity);
-            base.Put(id,entity);
+            _cachedRepository.Update(entity);
+            //base.Put(id,entity);
         }
 
         [HttpDelete("{id}")]
         public override void Delete([FromBody]T entity)
         {
-            _cached.Delete(entity);
-            base.Delete(entity);
+            _cachedRepository.Delete(entity);
+            //base.Delete(entity);
         }
     }
 }
