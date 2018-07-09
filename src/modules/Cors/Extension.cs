e021cd39bc45edd2d292e4b35a7bcc5d4a996b71
massimodipaolo <massimodipaolo@users.noreply.github.com>
2018-07-09 13:18:43 +0200

    Cors module

---
+++ src/modules/Cors/Extension.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace core.Extensions.Cors
+{
+    public class Extension : Base.Extension
+    {
+        private Options _options => GetOptions<Options>();
+        private IEnumerable<Options.PolicyOption> _namedPolicies => _options.Policies.Where(_ => !string.IsNullOrEmpty(_.Name));
+
+        public override void Execute(IServiceCollection serviceCollection, IServiceProvider serviceProvider)
+        {
+            base.Execute(serviceCollection, serviceProvider);
+
+            if (_namedPolicies.Any())
+            {
+                serviceCollection.AddCors(opt =>
+                {
+                    foreach (var p in _namedPolicies)
+                    {
+                        opt.AddPolicy(p.Name, _ =>
+                        {
+                            var _origins = p.Origins.Where(__ => !string.IsNullOrEmpty(__));
+                            if (_origins.Any()) { _.WithOrigins(_origins.ToArray()); } else { _.AllowAnyOrigin(); };
+
+                            var _methods = p.Methods.Where(__ => !string.IsNullOrEmpty(__));
+                            if (_methods.Any()) { _.WithMethods(_methods.ToArray()); } else { _.AllowAnyMethod(); };
+
+                            var _headers = p.Headers.Where(__ => !string.IsNullOrEmpty(__));
+                            if (_headers.Any()) { _.WithHeaders(_headers.ToArray()); } else { _.AllowAnyHeader(); };
+
+                            var _exposedHeaders = p.ExposedHeaders.Where(__ => !string.IsNullOrEmpty(__));
+                            if (_exposedHeaders.Any()) { _.WithExposedHeaders(_exposedHeaders.ToArray()); };
+
+                            if (p.AllowCredentials) { _.AllowCredentials(); } else { _.DisallowCredentials(); };
+
+                            if (p.PreflightMaxAgeInSeconds != null) _.SetPreflightMaxAge(TimeSpan.FromSeconds(p.PreflightMaxAgeInSeconds.Value));
+                        });
+                    }
+                });
+            }
+            else
+                serviceCollection.AddCors();
+
+        }
+
+        public override void Execute(IApplicationBuilder applicationBuilder, IServiceProvider serviceProvider)
+        {
+            base.Execute(applicationBuilder, serviceProvider);
+
+            if (_namedPolicies.Any())
+                foreach (var p in _namedPolicies)
+                    applicationBuilder.UseCors(p.Name);
+            else
+                applicationBuilder.UseCors(_ => _
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()                
+            );
+        }
+    }
+
+}
+
---
+++ src/modules/Cors/Options.cs
@@ -0,0 +1,20 @@
+﻿using core.Extensions.Base;
+
+namespace core.Extensions.Cors
+{
+    public class Options : IOptions
+    {
+        public PolicyOption[] Policies { get; set; }
+
+        public class PolicyOption
+        {
+            public string Name { get; set; }
+            public string[] Origins { get; set; }
+            public string[] Methods { get; set; }
+            public string[] Headers { get; set; }
+            public string[] ExposedHeaders { get; set; }
+            public bool AllowCredentials { get; set; } = false;
+            public int? PreflightMaxAgeInSeconds { get; set; }
+        }
+    }
+}
