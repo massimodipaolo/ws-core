7c794d6d6d2babf19954bd95876eb139076d4abd
massimodipaolo <massimodipaolo@users.noreply.github.com>
2018-07-09 13:33:43 +0200

    Cors project

---
+++ src/modules/Spa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
