0fa2db4c4dcd46f64cd740cb13c1f1e8a4177369
massimodipaolo <massimodipaolo@users.noreply.github.com>
2018-10-11 10:46:59 +0200

    Message: fix cast custom arguments

--- src/modules/Message/EmailMessage.cs
+++ src/modules/Message/EmailMessage.cs
@@ -36,7 +36,7 @@ namespace core.Extensions.Message
 
                 mime.Subject = message.Subject;
 
-                var model = (EmailMessageModel)message.Arguments ?? new EmailMessageModel();
+                var model = Newtonsoft.Json.JsonConvert.DeserializeObject<EmailMessageModel>(Newtonsoft.Json.JsonConvert.SerializeObject(message.Arguments));
 
                 var body = new TextPart(model.IsHtml ? TextFormat.Html : TextFormat.Plain)
                 {
