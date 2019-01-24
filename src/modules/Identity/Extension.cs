c1d1d3db8a77e92bd17f936251d7adc4b2dabd80
massimodipaolo <massimodipaolo@users.noreply.github.com>
2019-01-24 17:20:48 +0100

    Identity

--- src/modules/Identity/Extension.cs
+++ src/modules/Identity/Extension.cs
@@ -1,25 +1,40 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace core.Extensions.Identity
 {
-        public class Extension: Base.Extension
+    public class Extension: Base.Extension
     {
+        private Options _options => GetOptions<Options>();
+
         public override void Execute(IServiceCollection serviceCollection, IServiceProvider serviceProvider)
         {
             base.Execute(serviceCollection, serviceProvider);
-            serviceCollection.AddIdentityServer()                
-                                                .AddInMemoryClients(new IdentityServer4.Models.Client[] { new IdentityServer4.Models.Client() })
-                                                .AddInMemoryIdentityResources(new IdentityServer4.Models.IdentityResource[] { new IdentityServer4.Models.IdentityResource() });
+
+            var builder = serviceCollection.AddIdentityServer();
+            if (_options.InMemory != null)
+            {
+                builder
+                    .AddInMemoryIdentityResources(_options.InMemory.IdentityResources)
+                    .AddInMemoryApiResources(_options.InMemory.ApiResources)
+                    .AddInMemoryClients(_options.InMemory.Clients);
+
+                if (_options.InMemory.PersistedGrants)
+                    builder.AddInMemoryPersistedGrants();
+
+                if (_options.InMemory.Caching)
+                    builder.AddInMemoryCaching();
+            }
         }
 
         public override void Execute(IApplicationBuilder applicationBuilder, IServiceProvider serviceProvider)
         {
             base.Execute(applicationBuilder, serviceProvider);
+
             applicationBuilder.UseIdentityServer();
-        }
-        
+        }        
     }
 }
---
+++ src/modules/Identity/Options.cs
@@ -0,0 +1,21 @@
+﻿using core.Extensions.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using IdentityServer4.Models;
+
+namespace core.Extensions.Identity
+{
+    class Options : IOptions
+    {
+        public InMemoryOptions InMemory { get; set; }
+        public class InMemoryOptions
+        {
+            public IEnumerable<IdentityResource> IdentityResources { get; set; }
+            public IEnumerable<ApiResource> ApiResources { get; set; }
+            public IEnumerable<Client> Clients { get; set; }
+            public bool PersistedGrants { get; set; }
+            public bool Caching { get; set; }
+        }
+    }
+}
