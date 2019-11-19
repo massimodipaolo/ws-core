c1e3f6edc815b40c9e5720441d7a75af6e87a74f
massimodipaolo <massimodipaolo@users.noreply.github.com>
2019-11-19 11:39:24 +0100

    EF: fix ignore types

--- src/modules/Data.EF/AppDbContext.cs
+++ src/modules/Data.EF/AppDbContext.cs
@@ -30,15 +30,20 @@ namespace Ws.Core.Extensions.Data
                 .Ignore<Guid[]>()
                 .Ignore<string[]>();
 
-            // Ignore custom type      
+            // Ignore custom type    
+            var ignoreTypes = new List<Type>();
             if (options != null && options.Ignore != null && options.Ignore.Length > 0)
                 foreach (var type in options.Ignore)
                     try
                     {
                         Type t = Type.GetType(type);
                         if (t != null)
-                            modelBuilder.Ignore(t);
-                    } catch { }                    
+                        {
+                            ignoreTypes.Add(t);
+                            modelBuilder.Ignore(t);                            
+                        }
+                    }
+                    catch { }
 
             // JsonConvert: map field with text columun, serializing/deserializing value
             IEnumerable<Type> jsonConvertTypes = null;
@@ -55,62 +60,64 @@ namespace Ws.Core.Extensions.Data
 
             foreach (KeyValuePair<Type, int> tKey in tKeys)
             {
-                foreach (Type type in Base.Util.GetAllTypesOf(tKey.Key))
-                {
-                    try
-                    {
-                        EF.Options.MappingConfig opt = (options?.Mappings ?? new List<EF.Options.MappingConfig>())
-                        .FirstOrDefault(_ =>
-                            _.Name == type.Name
-                            && (string.IsNullOrEmpty(_.NameSpace) || _.NameSpace == type.Namespace)
-                        );
+                var types = Base.Util.GetAllTypesOf(tKey.Key).Where(t => !ignoreTypes?.Distinct().Any(i => i == t) == true);                
+                foreach (Type type in types)
+                {   
+                        try
+                        {
+                            EF.Options.MappingConfig opt = (options?.Mappings ?? new List<EF.Options.MappingConfig>())
+                            .FirstOrDefault(_ =>
+                                _.Name == type.Name
+                                && (string.IsNullOrEmpty(_.NameSpace) || _.NameSpace == type.Namespace)
+                            );
 
-                        // Map to db schema,table
-                        var entityBuilder = modelBuilder.Entity(type)
-                                    .ToTable(opt?.Table ?? type.Name, opt?.Schema ?? "dbo");
+                            // Map to db schema,table
+                            var entityBuilder = modelBuilder.Entity(type)
+                                        .ToTable(opt?.Table ?? type.Name, opt?.Schema ?? "dbo");
 
-                        // Map Id column
-                        entityBuilder.Property("Id").HasColumnName(opt?.IdColumnName ?? "Id")
-                                    .IsUnicode(false)
-                                    .HasMaxLength(tKey.Value)
-                                    //.HasColumnType(tKey.Value)
-                                    .HasDefaultValue();
+                            // Map Id column
+                            entityBuilder.Property("Id").HasColumnName(opt?.IdColumnName ?? "Id")
+                                        .IsUnicode(false)
+                                        .HasMaxLength(tKey.Value)
+                                        //.HasColumnType(tKey.Value)
+                                        .HasDefaultValue();
 
-                        // Map complex type (or interface) on a text column, serializing/deserializing value
-                        if (jsonConvertTypes != null && jsonConvertTypes.Any())
-                            foreach (var property in type.GetProperties()
-                                .Where(p => jsonConvertTypes
-                                    .Any(jT => jT.IsInterface ? jT.IsAssignableFrom(p.PropertyType) : jT == p.PropertyType)
+                            // Map complex type (or interface) on a text column, serializing/deserializing value
+                            if (jsonConvertTypes != null && jsonConvertTypes.Any())
+                                foreach (var property in type.GetProperties()
+                                    .Where(p => jsonConvertTypes
+                                        .Any(jT => jT.IsInterface ? jT.IsAssignableFrom(p.PropertyType) : jT == p.PropertyType)
+                                        )
                                     )
-                                )
-                                if (null == opt?.Properties?.FirstOrDefault(_ => _.Name==property.Name && _.JsonConvert.HasValue && _.JsonConvert.Value==false))
-                                    entityBuilder.Property(property.Name).HasJsonConversion(property.PropertyType);
+                                    if (null == opt?.Properties?.FirstOrDefault(_ => _.Name == property.Name && _.JsonConvert.HasValue && _.JsonConvert.Value == false))
+                                        entityBuilder.Property(property.Name).HasJsonConversion(property.PropertyType);
 
-                        // Property based settings
-                        if (opt?.Properties != null)
-                            foreach (var p in opt.Properties.Where(_ => !string.IsNullOrEmpty(_.Name)))
-                            {
-                                // Ignore field
-                                if (p.Ignore)
-                                    entityBuilder.Ignore(p.Name);
-                                else
-                                    try
-                                    {
-                                        // Custom map
-                                        if (!string.IsNullOrEmpty(p.Column))
-                                            entityBuilder.Property(p.Name).HasColumnName(p.Column);
-                                    }
-                                    catch { }
+                            // Property based settings
+                            if (opt?.Properties != null)
+                                foreach (var p in opt.Properties.Where(_ => !string.IsNullOrEmpty(_.Name)))
+                                {
+                                    // Ignore field
+                                    if (p.Ignore)
+                                        entityBuilder.Ignore(p.Name);
+                                    else
+                                        try
+                                        {
+                                            // Custom map
+                                            if (!string.IsNullOrEmpty(p.Column))
+                                                entityBuilder.Property(p.Name).HasColumnName(p.Column);
+                                        }
+                                        catch { }
 
-                                // Map specific property on a text column, serializing/deserializing value
-                                if (p.JsonConvert.HasValue && p.JsonConvert.Value == true)
-                                    entityBuilder.Property(p.Name).HasJsonConversion(type.GetProperty(p.Name).PropertyType);
-                            }
+                                    // Map specific property on a text column, serializing/deserializing value
+                                    if (p.JsonConvert.HasValue && p.JsonConvert.Value == true)
+                                        entityBuilder.Property(p.Name).HasJsonConversion(type.GetProperty(p.Name).PropertyType);
+                                }
 
-                    }
-                    catch(Exception ex) {
-                        throw ex;
-                    }
+                        }
+                        catch (Exception ex)
+                        {
+                            throw ex;
+                        }
                 }
             }
         }
