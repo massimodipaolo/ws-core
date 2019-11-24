9584af68c6e6b1ac1f2c3bc12331f32a4d7fc1fa
Massimo Di Paolo <massimodipaolo@users.noreply.github.com>
2019-11-24 20:30:39 +0100

    Health check .netcore 2.2

---
+++ src/modules/HealthCheck/Extension.cs
@@ -0,0 +1,52 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ws.Core.Extensions.HealthCheck
+{
+    public class Extension : Base.Extension
+    {
+        private Options _options => GetOptions<Options>() ?? new Options();
+        public override void Execute(IServiceCollection serviceCollection, IServiceProvider serviceProvider)
+        {
+            base.Execute(serviceCollection, serviceProvider);
+            var builder = serviceCollection.AddHealthChecks();
+            
+            // checks
+            var checks = _options.Checks;
+            if (checks != null)
+            {
+                // storage
+                if (checks.Storage != null && checks.Storage.Any())
+                    foreach(var storage in checks.Storage)
+                        builder.AddDiskStorageHealthCheck(_ => _.AddDrive(storage.Driver,storage.MinimumFreeMb), $"storage-{storage.Name}",storage.Status);
+
+                // tcp
+                if (checks.Tcp != null && checks.Tcp.Any())
+                    foreach (var tcp in checks.Tcp) 
+                        builder.AddTcpHealthCheck(_ => _.AddHost(tcp.Host,tcp.Port),$"tcp-{tcp.Name}",tcp.Status);
+                
+                // http
+                if (checks.Http != null && checks.Http.Any())
+                    foreach (var http in checks.Http)
+                        builder.AddUrlGroup(new Uri(http.Url), $"http-{http.Name}", http.Status);
+            }
+
+        }
+        public override void Execute(IApplicationBuilder applicationBuilder, IServiceProvider serviceProvider)
+        {
+            base.Execute(applicationBuilder, serviceProvider);
+            
+            applicationBuilder.UseHealthChecks(_options.Route, new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+        }
+    }
+}
---
+++ src/modules/HealthCheck/Options.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Ws.Core.Extensions.Base;
+
+namespace Ws.Core.Extensions.HealthCheck
+{
+    public class AuthOptions
+    {
+        public IEnumerable<string> AuthPolicies { get; set; }
+        public IEnumerable<string> AuthHosts { get; set; }
+    }
+    public class Options: AuthOptions,IOptions
+    { 
+        public string Route { get; set; } = "/healtz";
+        public CheckEntries Checks { get; set; }
+        public UiOptions Ui { get; set; } = new UiOptions();
+        public class CheckEntries
+        {
+            public IEnumerable<StorageCheck> Storage { get; set; }
+            public MemoryCheck Memory { get; set; }
+            public IEnumerable<ProcessCheck> Process { get; set; }
+            public IEnumerable<WinServiceCheck> WinService { get; set; }
+            public IEnumerable<TcpCheck> Tcp { get; set; }
+            public IEnumerable<HttpCheck> Http { get; set; }
+        }
+        public class ProcessCheck: HealthResult
+        {
+            public string ProcessName { get; set; }
+        }
+        public class WinServiceCheck : HealthResult
+        {
+            public string ServiceName { get; set; }
+        }
+        public class TcpCheck : HealthResult
+        {
+            public string Host { get; set; }
+            public int Port { get; set; }
+        }
+        public class MemoryCheck: HealthResult
+        {
+            public int MaximumAllocatedMb { get; set; }
+        }
+        public class HealthResult
+        {
+            public string Name { get; set; } = Guid.NewGuid().ToString().Substring(0, 8);
+            public Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus Status { get; set; } = Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Unhealthy;
+        }
+
+        public class HttpCheck : HealthResult
+        {
+            public string Url { get; set; }
+        }
+        public class StorageCheck: HealthResult
+        {
+         /// <summary>
+         /// i.e. C:\
+         /// </summary>
+            public string Driver { get; set; }
+            public long MinimumFreeMb { get; set; }
+        }
+
+        /// <summary>
+        /// https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks/blob/master/src/HealthChecks.UI/Configuration/Options.cs
+        /// </summary>
+        public class UiOptions: AuthOptions
+        {
+            public bool Enabled { get; set; } = false;
+            public string Route { get; set; } = "/healthchecks-ui";
+            public string RouteApi { get; set; } = "/healthchecks-api";
+            public string RouteWebhook { get; set; } = "/healthchecks-webhooks";
+            /// <summary>
+            /// i.e. wwwroot/healthcheck-ui/style.css
+            /// </summary>
+            public string InjectCss { get; set; }
+            public IEnumerable<EndpointOptions> Endpoints { get; set; }
+            public IEnumerable<WebhookOptions> Webhooks { get; set; }
+            public int EvaluationTimeinSeconds { get; set; } = 60;
+            public int MinimumSecondsBetweenFailureNotifications { get; set; } = 300;
+            public class EndpointOptions
+            {
+                public string Name { get; set; }
+                public string Uri { get; set; }
+            }
+            public class WebhookOptions: EndpointOptions
+            {
+                public string Payload { get; set; }
+                public string RestorePayload { get; set; }
+            }
+        }
+    }
+}
