2db17e904996f52287bfbd5539805ff60448502a
massimodipaolo <massimodipaolo@users.noreply.github.com>
2019-11-26 10:04:51 +0100

    Health check: skip check if empty identifier (i.e. tcp host, process name, service name...)

--- src/modules/HealthCheck/Extension.cs
+++ src/modules/HealthCheck/Extension.cs
@@ -23,17 +23,17 @@ namespace Ws.Core.Extensions.HealthCheck
             {
                 // storage
                 if (checks.Storage != null && checks.Storage.Any())
-                    foreach(var storage in checks.Storage)
+                    foreach(var storage in checks.Storage.Where(_ => !string.IsNullOrEmpty(_.Driver)))
                         builder.AddDiskStorageHealthCheck(_ => _.AddDrive(storage.Driver,storage.MinimumFreeMb), $"storage-{storage.Name}",storage.Status);
                 
                 // tcp
                 if (checks.Tcp != null && checks.Tcp.Any())
-                    foreach (var tcp in checks.Tcp) 
+                    foreach (var tcp in checks.Tcp.Where(_ => !string.IsNullOrEmpty(_.Host))) 
                         builder.AddTcpHealthCheck(_ => _.AddHost(tcp.Host,tcp.Port),$"tcp-{tcp.Name}",tcp.Status);
                 
                 // http
                 if (checks.Http != null && checks.Http.Any())
-                    foreach (var http in checks.Http)
+                    foreach (var http in checks.Http.Where(_ => !string.IsNullOrEmpty(_.Url)))
                         builder.AddUrlGroup(new Uri(http.Url), $"http-{http.Name}", http.Status);
             }
 
