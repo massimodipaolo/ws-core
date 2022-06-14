52ce00cf12ca0d316c657fb4d6830182f2dc62e0
Massimo Di Paolo <m.dipaolo@websolute.it>
2022-06-14 16:15:48 +0200

    Add sonarqube scan ps

---
+++ sonar-scan.ps1
@@ -0,0 +1,31 @@
+param(
+  [string]$configuration = 'Debug',
+  [string]$path = $PSScriptRoot,
+  [string[]]$targets = 'default'
+)
+
+# Read .env file: Install-Module -Name Set-PsEnv
+function prompt {
+    Set-PsEnv
+}
+
+prompt
+
+# Boostrap posh-build
+$build_dir = Join-Path $path ".build"
+if (! (Test-Path (Join-Path $build_dir "posh-build.ps1"))) { Write-Host "Installing posh-build..."; New-Item -Type Directory $build_dir -ErrorAction Ignore | Out-Null; Save-Script "posh-build" -Path $build_dir }
+. (Join-Path $build_dir "posh-build.ps1")
+
+# Set these variables as desired
+$solution_file = Join-Path $path "ws-core.sln";
+
+target default -depends scan
+
+target scan {
+  docker start $Env:SONAR_CONTAINER
+  dotnet sonarscanner begin /k:"ws-core-local" /d:sonar.host.url=$Env:SONAR_HOST_URL  /d:sonar.login=$Env:SONAR_LOGIN
+  dotnet build $solution_file -c $configuration --no-incremental
+  dotnet sonarscanner end /d:sonar.login=$Env:SONAR_LOGIN
+}
+
+Start-Build $targets
